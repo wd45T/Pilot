c6276d2586b7dc3b30f5b7870e9d82310f8abb0c
Artem Shpilevoy <wd45@yandex.ru>
2018-06-14 11:34:31 +0300

    add DB project

---
+++ Pilot/Pilot.DataCore/Infrastructure/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pilot.DataCore.Infrastructure
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+        DateTime Created { get; set; }
+        DateTime Modified { get; set; }
+        DateTime? Deleted { get; set; }
+    }
+}
---
+++ Pilot/Pilot.Database/dbo/Tables/User.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Id]			uniqueidentifier    NOT NULL,
+    [Name]			NVARCHAR (100) NOT NULL,
+	[Created]		DATETIME NOT NULL DEFAULT getdate(), 
+    [Modified]		DATETIME NOT NULL DEFAULT getdate(), 
+    [Deleted]		DATETIME NULL,
+    CONSTRAINT [PK_dbo.User] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+
+CREATE TRIGGER [dbo].[User_U_TR]
+   ON [dbo].[User] FOR UPDATE AS
+BEGIN
+ set nocount on
+  if not exists (select 1 from inserted) or 
+     not exists (select 1 from deleted) return
+update [User] set Modified=getdate()
+    from inserted i, deleted d, [User] upd_id
+   where i.Id = d.Id and d.Id=upd_id.Id
+ return       
+END
+GO
