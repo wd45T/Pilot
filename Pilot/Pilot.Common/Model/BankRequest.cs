ba539b917d2483a77e6e7a39400f974687d2c8e7
wd45T <Wd45@yandex.ru>
2018-07-02 23:26:53 +0300

    add icruid

---
+++ Pilot/Pilot.Common/Model/BankRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pilot.Common.Model
+{
+    public class BankRequest
+    {
+        public string FullNameBank { get; set; }
+        public string AddressBank { get; set; }
+        public string BIK { get; set; }
+        public string CorrespondingAccount { get; set; }
+    }
+}
---
+++ Pilot/Pilot.Repository/Interfaces/Common/ICRUIDRepository.cs
@@ -0,0 +1,17 @@
+﻿using Pilot.DataCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilot.Repository.Interfaces.Common
+{
+    public interface ICRUIDRepository<TEntity, TDto> : IDisposable where TEntity : IEntity where TDto : class
+    {
+        Task<ICollection<TDto>> GetDtoAll();
+        Task<TDto> GetDtoById(Guid id);
+        Task<int> Create(TEntity entity);
+        //Task<TEntity> GetEntityById(long id, string userLogin = null);
+        //Task<ICollection<TDto>> GetByIds(IEnumerable<long> ids, string userLogin = null);
+    }
+}
